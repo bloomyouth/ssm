88b079024c99707cf857c81d13ec9eab019b2197
bloomyouth <1019858427@qq.com>
2021-09-01 16:47:35 +0800

    完成前后端数据交互功能

--- src/main/java/com/wql/controller/MessageController.java
+++ src/main/java/com/wql/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.wql.controller;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.wql.pojo.Messages;
 import com.wql.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,26 +23,30 @@ public class MessageController {
     @Qualifier("MessageServiceImpl")
     private MessageService messageService;
 
-    @RequestMapping(value = "/allMessage")
+    @RequestMapping(value = "/allMessage",produces = "application/json;charset=utf-8")
     @ResponseBody
     public String queryAllMessage(){
-        System.out.println("接收到请求");
         List<Messages> list=messageService.queryAllMessage();
-//        String str=JSON.toJSONString(list);
-//        System.out.println(str);
-        System.out.println("转换完成");
-        return "wql";
+        String str = JSONArray.toJSONString(list);
+        System.out.println("ok");
+        return JSONArray.toJSONString(list);
     }
 
     @RequestMapping(value = "/test")
     public String test(){
-        Messages messages=new Messages();
-        messages.setId(1);
-        messages.setTime(new Date());
-        messages.setName("wql");
-        messages.setState("未认领");
-        messages.setType(1);
-        messageService.addMessage(messages);
+        List<Messages> list=new ArrayList<>();
+        for(int i=0;i<5;i++){
+            Messages messages=new Messages();
+            messages.setId(i+1);
+            messages.setTime(new Date());
+            messages.setName("wql");
+            messages.setState("未认领");
+            messages.setType(1);
+            list.add(messages);
+        }
+        String jsonString = JSONArray.toJSONString(list);
+        JSONArray jsonArray = JSONArray.parseArray(jsonString);
+        System.out.println(jsonArray);
         System.out.println("成功");
         return "test";
     }
